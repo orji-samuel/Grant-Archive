a38ad406b5fecf4e047b9eb5303eff5a55678179
orji-samuel <orjis553@gmail.com>
2025-09-15 15:36:52 +0200

    first commit

---
+++ gth.py
@@ -0,0 +1,14 @@
+
+# all of the following are equivalent
+my_string = 'Hello'
+print(my_string)
+my_string = "Hello"
+my_string
+my_string = '''Hello'''
+my_string
+# triple quotes string can extend multiple lines
+my_string = """Hello, welcome to
+ the world of Python"""
+c=" mlritm"
+# substring function
+my_string[5:11]
